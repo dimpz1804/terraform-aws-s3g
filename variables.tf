377b1cb0dd3e3277fe9c0c88a6f919c5b9994691
dimpz1804 <dimpy.chugh@hashicorp.com>
2025-01-23 12:20:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  description = " "
+}
+
+variable "app_name" {
+  description = " "
+}
+
+variable "tags" {
+  description = "A map of tags to be applied to resources"
+  default     = {}
+}
